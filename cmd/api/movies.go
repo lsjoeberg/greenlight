6e851c1e71d589d2cd9a9876490fb6e3e3a604ea
Lars Sjöberg <lars.sjoberg@volvocars.com>
2023-03-04 15:16:45 +0100

    Refactor movie ID checking

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 // createMovieHandler handles the "POST /v1/movies" endpoint.
@@ -15,11 +12,10 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 
 // showMovieHandler handles the "GET /v1/movies/:id" endpoint.
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	params := httprouter.ParamsFromContext(r.Context())
 
 	// Check if movie ID is valid.
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
