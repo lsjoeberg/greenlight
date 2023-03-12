7838b92fb41ce5a81f960aed16923f7e832af7de
Lars Sjöberg <lars.sjoberg@volvocars.com>
2023-03-12 14:29:14 +0100

    Implement deleteMovie

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -117,5 +117,27 @@ func (m MovieModel) Update(movie *Movie) error {
 
 // Delete removes a specific record from the movies table.
 func (m MovieModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	query := `
+		DELETE FROM movies
+		WHERE id = $1`
+
+	// Delete movie db record.
+	result, err := m.DB.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+
 	return nil
 }
