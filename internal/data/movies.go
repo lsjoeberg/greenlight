f9aca7e1d1ee5d1ebdfd5a926f95a2f176a949f5
Lars Sjöberg <lars.sjoberg@volvocars.com>
2023-03-12 14:11:17 +0100

    Implement updateMovie

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -107,7 +107,12 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 
 // Update updates a specific record in the movies table.
 func (m MovieModel) Update(movie *Movie) error {
-	return nil
+	query := `
+		UPDATE movies SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1 
+		WHERE id = $5 RETURNING version`
+
+	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres), movie.ID}
+	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
 }
 
 // Delete removes a specific record from the movies table.
