baed2a6ef75b2c7373cff54edcdba8dd760848eb
Lars Sjöberg <lars.sjoberg@volvocars.com>
2023-03-19 17:50:31 +0100

    Sort movie list based on user parameter

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,10 @@
 package data
 
-import "github.com/lsjoeberg/greenlight/internal/validator"
+import (
+	"strings"
+
+	"github.com/lsjoeberg/greenlight/internal/validator"
+)
 
 type Filters struct {
 	Page         int
@@ -18,4 +22,24 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 
 	// Check that the sort parameter matches a value in the safelist.
 	v.Check(validator.In(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+}
+
+// sortColumn compares client-provided Sort field with permissible values.
+// If the parameter value is valid, it's extracted and returned.
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+// sortDirection returns the sort direction ("ASC" or "DESC") depending on the
+// prefix character of the Sort field.
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
 }
