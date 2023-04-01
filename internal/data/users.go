7a60dce0a2d16c998693bd5e8c4152c920f52ac8
Lars Sjöberg <lars.sjoberg@volvocars.com>
2023-04-01 17:06:10 +0200

    Add User entity

---
+++ internal/data/users.go
@@ -0,0 +1,52 @@
+package data
+
+import (
+	"errors"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// User represents an individual user.
+type User struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  password  `json:"-"`
+	Activated bool      `json:"activated"`
+	Version   int       `json:"-"`
+}
+
+// password holds the plaintext and hashed versions of a password.
+type password struct {
+	plaintext *string
+	hash      []byte
+}
+
+// Set calculates the bcrypt hash of a plaintext password, and stores both
+// the hash and the plaintext versions in the struct.
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+	if err != nil {
+		return err
+	}
+	p.plaintext = &plaintextPassword
+	p.hash = hash
+	return nil
+}
+
+// Matches checks whether the provided plaintext password matches the hashed
+// password stored in the struct, returning true if it matches and false otherwise.
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+	return true, nil
+}
