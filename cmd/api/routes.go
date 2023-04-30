0671d45ebb4c36fc07cf630151694a96e4319ac2
Lars Sjöberg <lars.sjoberg@volvocars.com>
2023-04-30 16:26:31 +0200

    Add authn token creation route

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -26,6 +26,9 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
+	// Authentication routes.
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+
 	// Middleware chain.
 	return app.recoverPanic(app.rateLimit(router))
 }
