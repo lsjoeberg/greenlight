a80fb040777b1ec95ff633073bf862647fd85b3f
Lars Sjöberg <lars.sjoberg@volvocars.com>
2023-05-07 16:57:37 +0200

    Add expvar metrics

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"expvar"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -28,6 +29,9 @@ func (app *application) routes() http.Handler {
 
 	// Authentication routes.
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+
+	// Metrics.
+	router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
 
 	// Middleware chain.
 	return app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router))))
